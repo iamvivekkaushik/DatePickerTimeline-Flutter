50f8d21d654c1ff9e00e4de4372d79b06fb8af22
hamza aboshhiwa <42789638+Hamza-Shewa@users.noreply.github.com>
2024-11-04 13:49:01 +0200

    fix deprication

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\HamzaB\Downloads\work space\DatePickerTimelineFlutter\example"
+export "FLUTTER_ROOT=C:\development\flutter"
+export "FLUTTER_APPLICATION_PATH=F:\work_space\DatePickerTimelineFlutter\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
